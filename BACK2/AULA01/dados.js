91885b7a1700da93079f3951009d5eba4159e747
arthur007cesf <162136883+arthur007cesf@users.noreply.github.com>
2024-04-05 09:05:14 -0300

    Add files via upload

---
+++ BACK2/AULA01/dados.js
@@ -0,0 +1,12 @@
+//criar um array de nomes
+
+let nomes =["Admilson","Berenilda","Ceciliano"];
+let frutas=["amora","banana","caqui"];
+
+function soma(n1,n2){
+    return n
+}
+
+//module.exports = frutas;
+
+module.exports(nomes,frutas,soma);
